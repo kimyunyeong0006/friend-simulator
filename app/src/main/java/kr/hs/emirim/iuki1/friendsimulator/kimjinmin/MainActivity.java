3dbddc57ef2bc02cc7f86fe3e0063b773a803c38
kimyunyeong0006 <iuki1@e-mirim.hs.kr>
2017-04-06 19:40:40 +0900

    #1 리니어 레이아웃으로 레이아웃 변경

---
+++ app/src/main/java/kr/hs/emirim/iuki1/friendsimulator/kimjinmin/MainActivity.java
@@ -0,0 +1,12 @@
+package kr.hs.emirim.iuki1.friendsimulator.kimjinmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
