ae6b97ef90d5eed8d749d2815bb05a86960dd8e6
kimyunyeong0006 <iuki1@e-mirim.hs.kr>
2017-04-10 20:54:05 +0900

    #12 평서상태 이미지 넣고 참조만들기

--- app/src/main/java/kr/hs/emirim/iuki1/friendsimulator/kimjinmin/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/iuki1/friendsimulator/kimjinmin/MainActivity.java
@@ -1,12 +1,43 @@
 package kr.hs.emirim.iuki1.friendsimulator.kimjinmin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String TAG = "1";
+    private ImageView mImageViewFriendVisual;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate 메소드 호출");
         setContentView(R.layout.activity_main);
+        mImageViewFriendVisual = (imageView) findViewById(R.id.friend_visual);
+        Log.d(TAG, "activity_main 레이아웃 세팅");
     }
+
+    void onClick(View view){
+        Log.d(TAG, "onClick 메소드 호출");
+        Log.d(TAG, "클릭된 뷰");
+        switch (view.getId()){
+            case R.id.button1:
+                Toast.makeText(this,"미쳤나봐", Toast.LENGTH_SHORT).show(); break;
+            case R.id.button2:
+                Toast.makeText(this,"음⊙_⊙", Toast.LENGTH_SHORT).show();break;
+            case R.id.button3:
+                Toast.makeText(this,"엥", Toast.LENGTH_SHORT).show();break;
+            case R.id.button4:
+                Toast.makeText(this,"미아넹", Toast.LENGTH_SHORT).show();break;
+            case R.id.button5:
+                Toast.makeText(this,"으흠", Toast.LENGTH_SHORT).show();break;
+            default: Toast.makeText(this,"다시선택해", Toast.LENGTH_SHORT).show();
+        }
+
+        this.view = view;
+    }
+}
