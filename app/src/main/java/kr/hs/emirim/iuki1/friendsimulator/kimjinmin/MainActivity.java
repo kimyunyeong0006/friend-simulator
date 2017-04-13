081fc930550b5bd64016e1c6c3a504f2e7c0a7f3
kimyunyeong0006 <iuki1@e-mirim.hs.kr>
2017-04-13 20:42:55 +0900

    first commit

--- app/src/main/java/kr/hs/emirim/iuki1/friendsimulator/kimjinmin/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/iuki1/friendsimulator/kimjinmin/MainActivity.java
@@ -1,6 +1,8 @@
 package kr.hs.emirim.iuki1.friendsimulator.kimjinmin;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     public final static String TAG = "1";
     private ImageView mImageViewFriendVisual;
     private View view;
+    private MediaPlayer mMidiaPlayer;
+    private Vibrator mVib;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,28 +24,63 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mImageViewFriendVisual = (ImageView) findViewById(R.id.friend_visual);
         Log.d(TAG, "activity_main 레이아웃 세팅");
+        mMidiaPlayer = MediaPlayer.create(this, R.raw.a03);
+        mMidiaPlayer.setLooping(false);
+        mMidiaPlayer.start();
+        mVib = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        mVib.vibrate(2000);
     }
 
-    void onClick(View view){
+    protected void onDestroy() {
+        super.onDestroy();
+        mMidiaPlayer.pause();
+        mMidiaPlayer.stop();
+    }
+
+
+    void onClick(View view) {
         Log.d(TAG, "onClick 메소드 호출");
         Log.d(TAG, "클릭된 뷰");
-        switch (view.getId()){
+        mVib.vibrate(500);
+        if (mMidiaPlayer.isPlaying()) {
+            mMidiaPlayer.pause();
+        }
+
+        switch (view.getId()) {
             case R.id.button1:
                 mImageViewFriendVisual.setImageResource(R.drawable.jm_01);
-                Toast.makeText(this,"미쳤나봐", Toast.LENGTH_SHORT).show(); break;
+                Toast.makeText(this, "미쳤나봐", Toast.LENGTH_SHORT).show();
+                mMidiaPlayer = MediaPlayer.create(this, R.raw.a01);
+                mMidiaPlayer.start();
+                break;
             case R.id.button2:
                 mImageViewFriendVisual.setImageResource(R.drawable.jm_02);
-                Toast.makeText(this,"음⊙_⊙", Toast.LENGTH_SHORT).show();break;
+                Toast.makeText(this, "음⊙_⊙", Toast.LENGTH_SHORT).show();
+                mMidiaPlayer = MediaPlayer.create(this, R.raw.a02);
+                mMidiaPlayer.start();
+                break;
             case R.id.button3:
                 mImageViewFriendVisual.setImageResource(R.drawable.jm_03);
-                Toast.makeText(this,"엥", Toast.LENGTH_SHORT).show();break;
+                Toast.makeText(this, "엥", Toast.LENGTH_SHORT).show();
+                mMidiaPlayer = MediaPlayer.create(this, R.raw.a03);
+                mMidiaPlayer.start();
+                break;
             case R.id.button4:
                 mImageViewFriendVisual.setImageResource(R.drawable.jm_04);
-                Toast.makeText(this,"미아넹", Toast.LENGTH_SHORT).show();break;
+                Toast.makeText(this, "미아넹", Toast.LENGTH_SHORT).show();
+                mMidiaPlayer = MediaPlayer.create(this, R.raw.a04);
+                mMidiaPlayer.start();
+                break;
             case R.id.button5:
                 mImageViewFriendVisual.setImageResource(R.drawable.jm_05);
-                Toast.makeText(this,"으흠", Toast.LENGTH_SHORT).show();break;
-            default: Toast.makeText(this,"다시선택해", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "으흠", Toast.LENGTH_SHORT).show();
+                mMidiaPlayer = MediaPlayer.create(this, R.raw.a05);
+                mMidiaPlayer.start();
+                break;
+            default:
+                Toast.makeText(this, "다시선택해", Toast.LENGTH_SHORT).show();
+                mMidiaPlayer = MediaPlayer.create(this, R.raw.a01);
+                mMidiaPlayer.start();
         }
 
         this.view = view;
